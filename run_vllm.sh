2e72091c7e5d43b75a535660dfe0cbe43d900bd9
Begunner <went@bytedance.com>
2025-12-19 20:15:31 +0800

    modified dockerfile

--- run_vllm.sh
+++ run_vllm.sh
@@ -4,9 +4,9 @@ docker run --gpus all -it --rm \
     -v $(pwd):/workspace \
     -v /data00/models:/workspace/models \
     -w /workspace \
-    -e ROLLOUT_NAME=sglang \
+    -e ROLLOUT_NAME=vllm \
     -e SGL_DISABLE_TP_MEMORY_INBALANCE_CHECK=True \
     -e NCCL_SHM_DISABLE=1 \
     -e NCCL_P2P_DISABLE=1 \
-    wb-sglang:latest \
+    wb-vllm/latest:latest \
     bash
