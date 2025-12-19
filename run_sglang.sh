2c5f73ea3cee72ff4778a8962be55a0dd2e43a5c
root <root@bytedance>
2025-12-19 16:47:58 +0800

    runsg

--- run_sglang.sh
+++ run_sglang.sh
@@ -2,11 +2,11 @@ docker run --gpus all -it --rm \
     --ipc=host \
     --network=host \
     -v $(pwd):/workspace \
+    -v /data00/models:/workspace/models
     -w /workspace \
     -e ROLLOUT_NAME=sglang \
     -e SGL_DISABLE_TP_MEMORY_INBALANCE_CHECK=True \
     -e NCCL_SHM_DISABLE=1 \
     -e NCCL_P2P_DISABLE=1 \
-    -e HF_ENDPOINT=https://hf-mirror.com \
-    your-verl-image:latest \
+    wb-sglang:latest \
     bash
