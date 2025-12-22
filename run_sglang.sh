cc06809dc4037e58095703cf009d8cca75d0ea43
Begunner <went@bytedance.com>
2025-12-22 18:32:36 +0800

    reproduced bug: sglang import vllm

--- run_sglang.sh
+++ run_sglang.sh
@@ -3,8 +3,10 @@ docker run --gpus all -it --rm \
     --network=host \
     -v $(pwd):/workspace \
     -v /data00/models:/workspace/models \
+    -v /data00/datasets:/workspace/datasets \
     -w /workspace \
     -e ROLLOUT_NAME=sglang \
+    -e PYTHONPATH=$PYTHONPATH:. \
     -e SGL_DISABLE_TP_MEMORY_INBALANCE_CHECK=True \
     -e NCCL_SHM_DISABLE=1 \
     -e NCCL_P2P_DISABLE=1 \
