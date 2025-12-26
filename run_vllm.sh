48e9ab4191e105b62e34addc7d4a97c6c69ff811
Begunner <went@bytedance.com>
2025-12-27 00:33:01 +0800

    update scripts

--- run_vllm.sh
+++ run_vllm.sh
@@ -3,6 +3,8 @@ docker run --gpus all -it --rm \
     --network=host \
     -v $(pwd):/workspace \
     -v /data00/models:/workspace/models \
+    -v /data00/datasets:/workspace/datasets \
+    -v /data00/checkpoints:/workspace/ckpts \
     -w /workspace \
     -e PYTHONPATH=$PYTHONPATH:. \
     -e ROLLOUT_NAME=vllm \
